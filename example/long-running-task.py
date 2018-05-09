ad7e6e2a6e68daa0174a10dd2225b7205182ac8a
David Woodruff <david_woodruff@byu.edu>
2018-05-09 16:43:39 -0600

    Add example

---
+++ example/long-running-task.py
@@ -0,0 +1,6 @@
+import time
+
+if __name__ == '__main__':
+    print('Starting some long-running task')
+    time.sleep(60) # Simulate some delayed task
+    print('Finished some long-running task')
